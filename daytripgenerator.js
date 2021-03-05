cf2b4a848951ce4bbb0a61727fce637e1604c962
unknown <ssgsj.carranza@gmail.com>
2021-03-05 12:35:51 -0600

    added user prompt, and united the functions, from lines 36-54

--- daytripgenerator.js
+++ daytripgenerator.js
@@ -33,5 +33,26 @@ function randomEntertainment(){
 console.log(randomEntertainment());
 
 
+function generateValue(){
+    let destination= randomDestination()
+    let restaurant= randomRestaurant()
+    let transport= randomTransport()
+    let entertainment= randomEntertainment()
+    return destination + " " + restaurant+ " " + transport+ " " + entertainment
+}
+
+
+let r= prompt("Do you want to re-select?" +" " +generateValue());
+    if(r=="yes"){
+        console.log(r);
+        while(r == "yes"){
+            r= prompt("Do you want to re-select?" +" " +generateValue());
+        }
+    }
+    else{
+        console.log("complete");
+    }
+   
+
 
 
