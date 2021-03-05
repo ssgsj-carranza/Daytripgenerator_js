981618f064ecf3425328c4fc7515babd3dc7e8ad
unknown <ssgsj.carranza@gmail.com>
2021-03-05 11:58:12 -0600

    added function random entertainment, liners 28-33

--- daytripgenerator.js
+++ daytripgenerator.js
@@ -7,7 +7,30 @@ console.log(randomDestination());
 
 
 
+function randomRestaurant(){
+    const restaurant=["mcdonalds", "tacobell", "burgerking"];
+    const randomRestaurant= Math.floor(Math.random()*(3));
+    return restaurant[randomRestaurant];
+}
+console.log(randomRestaurant());
 
+
+
+function randomTransport(){
+    const transport=["bike", "car", "piggyback"];
+    const randomTransport= Math.floor(Math.random()*(3));
+    return transport[randomTransport];
+}
+console.log(randomTransport());
+
+
+
+function randomEntertainment(){
+    const entertainment=["musical","movies","yugioh cards"];
+    const randomEntertainment= Math.floor(Math.random()*(3));
+    return entertainment[randomEntertainment];
+}
+console.log(randomEntertainment());
 
 
 
